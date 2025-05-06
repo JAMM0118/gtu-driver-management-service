3b77094dd2fddd4def61a82f1e82e741a37632c5
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2025-05-05 19:09:10 -0500

    first commit

---
+++ src/main/java/com/gtu/driver_management_service/DriverManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gtu.driver_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverManagementServiceApplication.class, args);
+	}
+
+}
