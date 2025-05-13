cbd32169ac0adce0194ef23152a7c9873a31bf60
Pbernal30980 <pbernal309@gmail.com>
2025-05-13 16:18:01 -0500

    Remove unnecessary imports and clean up the code in RouteAssignmentEntity.

--- src/main/java/com/gtu/driver_management_service/infrastructure/persistence/entity/RouteAssignmentEntity.java
+++ src/main/java/com/gtu/driver_management_service/infrastructure/persistence/entity/RouteAssignmentEntity.java
@@ -1,9 +1,6 @@
 package com.gtu.driver_management_service.infrastructure.persistence.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.UUID;
 
 @Entity
@@ -16,23 +13,27 @@ public class RouteAssignmentEntity {
     private UUID driverId;
     private UUID routeId;
 
-    
     // Getters y setters
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public UUID getDriverId() {
         return driverId;
     }
+
     public void setDriverId(UUID driverId) {
         this.driverId = driverId;
     }
+
     public UUID getRouteId() {
         return routeId;
     }
+
     public void setRouteId(UUID routeId) {
         this.routeId = routeId;
     }
